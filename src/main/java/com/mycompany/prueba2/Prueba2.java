c349edddef1f4b9997a3a25c27736b711fef6b32
itzesc0701 <itzelcordeo2007@gmail.com>
2023-06-28 10:20:14 -0600

    Mi primer commit

---
+++ src/main/java/com/mycompany/prueba2/Prueba2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
